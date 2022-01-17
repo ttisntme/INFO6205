136c44013d71fe2cad46cb223f466a62cf983c19
ttisntme <huang.yuan1@northeastern.edu>
2022-01-17 15:13:12 -0500

    assignment 0, run Newton.java with own function

--- src/main/java/edu/neu/coe/info6205/functions/Newton.java
+++ src/main/java/edu/neu/coe/info6205/functions/Newton.java
@@ -52,7 +52,8 @@ public class Newton {
     public static void main(String[] args) {
 
         // Build the Newton's Approximation problem to be solved: cos(x) = x
-        Newton newton = new Newton("cos(x) - x", (double x) -> Math.cos(x) - x, (double x) -> -Math.sin(x) - 1);
+//        Newton newton = new Newton("cos(x) - x", (double x) -> Math.cos(x) - x, (double x) -> -Math.sin(x) - 1);
+        Newton newton = new Newton("sin(x) - x", (double x) -> Math.sin(x) - x, (double x) -> Math.cos(x) - 1);
 
         // Solve the problem starting with a value of x = 1;
         // requiring a precision of 10^-7;
